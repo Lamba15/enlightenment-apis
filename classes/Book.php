9b060ea3d8ebd879d77e5dd6227681d8c1a8bd9f
lamba <programmer.lamba@gmail.com>
2022-11-11 02:23:31 +0200

    added added get books and add book functions to book class

--- classes/Book.php
+++ classes/Book.php
@@ -2,8 +2,10 @@
 
 namespace classes;
 
+use PDO;
+
 class Book {
-	public int $id;
+	public int|null $id;
 	public string $name;
 	public string $author;
 	public string $description;
@@ -12,4 +14,49 @@ class Book {
 	public int $stock;
 	public string $dateCreated;
 	public int $visits;
+
+	public function __construct(array $data) {
+		$this->id = $data["id"] ?? null;
+		$this->name = $data["name"];
+		$this->author = $data["author"];
+		$this->description = $data["description"];
+		$this->price = $data["price"];
+		$this->imageUrl = $data["imageUrl"];
+		$this->stock = $data["stock"];
+		$this->dateCreated = $data["dateCreated"];
+		$this->visits = $data["visits"];
+	}
+
+	/**
+	 * @param PDO $db
+	 *
+	 * @return array<Book>
+	 */
+	public static function getBooks(PDO $db): array {
+		$sql = "SELECT * from books";
+		$stmt = $db->prepare($sql);
+		$stmt->execute([]);
+
+		$books = [];
+
+		while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+			$books[] = new Book($row);
+		}
+
+		return $books;
+	}
+
+	public static function addBook(PDO $db, Book $book): Response {
+		$sql = "INSERT INTO books 
+    				(name, description, price, imageUrl, stock)
+					values (:name, :description, :price, :imageUrl, :stock)";
+		$stmt = $db->prepare($sql);
+		if ($stmt->execute(["name" => $book->name, "description" => $book->description, "price" => $book->price,
+							"imageUrl" => $book->imageUrl, "stock" => $book->stock])){
+			return new Response(1, "Successfully added a book");
+		} else {
+			return new Response(1, "Failed to add a book");
+		}
+	}
+
 }
